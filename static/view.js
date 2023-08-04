0891121309967cf5a4eaa2958d30121190936c09
Madhav Kumar <madhavampire@gmail.com>
2023-08-04 16:28:04 +0530

    save point 2

--- static/view.js
+++ static/view.js
@@ -1,7 +1,7 @@
 let page = 1;
 
 function loadFiles() {
-    $.get('/files?page=' + page, function(data) {
+    $.get('/files?page=' + page, function (data) {
         $('#fileContainer').html(data);
         page++;
 
@@ -14,26 +14,36 @@ function loadFiles() {
     });
 }
 
-$(document).ready(function() {
-    $('#openModal').click(function() {
+$(document).ready(function () {
+    $('#openModal').click(function () {
         loadFiles();
         $('#fileModal').modal();
     });
 
-    $('#loadMore').click(function() {
+    $('#loadMore').click(function () {
         loadFiles();
     });
 
-    $(document).on('click', '.download', function() {
+    $(document).on('click', '.download', function () {
         let fileId = $(this).data('id');
         $.ajax({
             url: '/download',
             type: 'POST',
             contentType: 'application/json',
             data: JSON.stringify({ fileId: fileId }),
-            success: function(data) {
+            success: function (data) {
                 window.open(data.downloadLink, '_blank');
             }
         });
+    });
+});
+
+$(document).on('click', '.download', function (e) {
+    e.preventDefault();  // Prevent the default form submission behavior
+
+    let fileId = $(this).data('id');
+
+    $.post('/download', { fileId: fileId }, function (data) {
+        console.log(data);  // Log the server response to the console
     });
 });
